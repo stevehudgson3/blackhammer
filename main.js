1c451a48808d62ff197526227ef7ad85c695e1cd
Steve Hudgson <steve.allen96@hotmail.com>
2020-01-21 00:49:38 -0600

    initial files from esports

---
+++ main.js
@@ -0,0 +1,9 @@
+// $(".jumbotron").css({
+//     height:$(window).height() + "px"
+// });
+
+// $(window).on("resize", function() {
+//     $(".jumbotron").css({
+//         height: $(window).height() + "px"
+//     });
+// });
